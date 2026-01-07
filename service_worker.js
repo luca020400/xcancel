254baf730bd74599bb17bf956e967b530a07492c
Luca Stefani <luca.stefani.ge1@gmail.com>
2026-01-07 12:49:25 +0100

    Remove service and perms

--- service_worker.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((e) => {
-  const msg = `Navigation to ${e.request.url} redirected on tab ${e.request.tabId}.`;
-  console.log(msg);
-});
-
-console.log('Service worker started.');
